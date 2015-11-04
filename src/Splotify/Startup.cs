45671eeb2680baa99d0431ed2e49874e9f007446
K. Scott Allen <scott@odetocode.com>
2015-11-04 11:13:20 -0500

    basic start

---
+++ src/Splotify/Startup.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNet.Builder;
+using Microsoft.Framework.DependencyInjection;
+
+namespace Splotify
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseIISPlatformHandler();
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+        }
+    }
+}
